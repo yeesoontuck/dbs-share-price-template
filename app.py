a2c4c674ab61fbba7a4a5343230fe477ad50b163
Yee | DSAI1 <yeesoontuck@gmail.com>
2025-05-12 16:50:08 +0800

    added DBS csv data file added Jupyter notebook with Linear Regression model updated coefficient and y-intercept as variables

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 
+# from linear regression model
+coefficient = -50.6
+y_intercept = 90.2
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
@@ -9,7 +13,7 @@ def index():
 @app.route("/prediction", methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
-    return(render_template("prediction.html", r=(-50.6*q)+90.2))
+    return(render_template("prediction.html", r=(q * coefficient) + y_intercept))
 
 if __name__ == "__main__":
     app.run()
